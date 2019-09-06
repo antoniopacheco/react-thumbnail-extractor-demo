62e0907e39816435226d1341e9e43fd2d18a0616
jesus antonio pacheco morales <antonio_pacheco@outlook.com>
2019-09-05 21:46:24 -0700

    demo page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ThumbnailExtractor from 'react-thumbnail-extractor'
+import { throwStatement } from '@babel/types';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      file:  null,
+      images: []
+    }
+  }
+
+  handleChange = (event) =>{
+    this.setState({
+      file: event.target.files[0]
+    })
+  }
+  onBeforeCapture = (event) => {
+    console.log("before capture",event);
+  }
+  onStartCapture = (event) => {
+    console.log("onStartCapture ",event);
+  }
+  onUnsupportedVideo = (event) => {
+    console.log("onUnsupportedVideo ",event);
+  }
+
+  onCapture = (event) => {
+    console.log("onCapture ",event);
+    this.setState({
+      images: event
+    })
+  }
+
+  onComplete = (event) => {
+    console.log("onComplete ",event);
+  }
+
+  onCompleteDetails = (event) => {
+    console.log("onCompleteDetails ",event);
+  }
+
+
+  
+  render() {
+    const { file, images } = this.state
+    return (
+      <div className="App">
+        <header className="App-header"><h1>ThumbnailExtractor Demo</h1></header>
+        <input type="file" onChange={this.handleChange} accept="video/mp4,video/x-m4v,video/*"></input>
+        <div className="imageContainer">
+        <ThumbnailExtractor displayImages maxWidth={600} onCompleteDetails={this.onCompleteDetails} onComplete={this.onComplete} onCapture={this.onCapture} onUnsupportedVideo={this.onUnsupportedVideo}  onBeforeCapture={this.onBeforeCapture} onStartCapture={this.onStartCapture} videoFile={this.state.file} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
